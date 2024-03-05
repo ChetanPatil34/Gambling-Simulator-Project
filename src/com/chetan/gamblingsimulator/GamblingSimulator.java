e2d79e706d34e4794ec6f5eec41ddf307a2c7900
ChetanPatil34 <cp9210639@gmail.com>
2024-03-05 12:45:50 +0530

    [ADDED] : CHETAN Simulator Project

---
+++ src/com/chetan/gamblingsimulator/GamblingSimulator.java
@@ -0,0 +1,10 @@
+package com.chetan.gamblingsimulator;
+
+public class GamblingSimulator {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Well Come to Gambling Simulator Project");
+	}
+
+}
