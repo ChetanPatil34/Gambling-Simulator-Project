3de8c64247cb354a511709e8a4a8210f2ef23d68
ChetanPatil34 <cp9210639@gmail.com>
2024-03-06 11:48:57 +0530

    [MODIFIED]:Chetan - UC-1 Complate for setting stack

--- src/com/chetan/gamblingsimulator/GamblingSimulator.java
+++ src/com/chetan/gamblingsimulator/GamblingSimulator.java
@@ -1,10 +1,23 @@
 package com.chetan.gamblingsimulator;
 
 public class GamblingSimulator {
+	
+	
+	private static final int EVERY_DAY_STAKE = 100;
+	private static final int PER_GAME_STAKE = 1;
+	
+	public static void gameStatus()
+	{
+		System.out.println("Every Day Stake:"+EVERY_DAY_STAKE);
+		System.out.println("Per Game Stake:"+PER_GAME_STAKE);
+		
+	}
 
 	public static void main(String[] args) {
 		
 		System.out.println("Well Come to Gambling Simulator Project");
+		
+		gameStatus();
 	}
 
 }
