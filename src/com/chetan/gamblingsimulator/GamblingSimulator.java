b2cb2cbe4946c90309605b509fabc7df8d92c418
ChetanPatil34 <cp9210639@gmail.com>
2024-03-09 11:34:54 +0530

    [MODIFIED]:Chetan - UC-4 Playing 20 days total amount of Win and loose

--- src/com/chetan/gamblingsimulator/GamblingSimulator.java
+++ src/com/chetan/gamblingsimulator/GamblingSimulator.java
@@ -5,41 +5,54 @@ public class GamblingSimulator {
 	
 	private static final int EVERY_DAY_STAKE = 100;
 	private static final int PER_GAME_STAKE = 1;
+	//private static final int MONTH_DAYS = 20;
 	
 	private static final int WIN = 1;
 	private static final int LOOSE = 0;
 	
-	private static int winstack = 0;
-	private static int loosestack = 0;
 	
+	
+	private static final int[] winAmount = new int[20];
+	private static final int[] looseAmount = new int[20];
+	
+	
+	static int playstatus;
 	
 	public static void gameStatus()
 	{
 		System.out.println("Every Day Stake:"+EVERY_DAY_STAKE);
 		System.out.println("Per Game Stake:"+PER_GAME_STAKE);
-		System.out.println("winning stack:"+winstack);
-		System.out.println("Loose Stack:"+loosestack);
-		System.out.println("-----------------------------");
+		
+		
 	}
-	public static void playgame()
+	public static void playgame() 
 	{
+			
+		
+		for(int i=0; i<20; i++)
+		{
+			System.out.println("Day :" + (i+1));
+			
+		    
+			int winstack = 0;
+		    int loosestack = 0;
 		
 		while(true) {
-			gameStatus();	
-		
+			
+			
 		Random randam = new Random();
-		int playstatus = randam.nextInt(9)%2;
+	    playstatus = randam.nextInt(9)%2;
 	   
-		System.out.println("Play status:"+playstatus);
+		//System.out.println("Play status:"+playstatus);
 		
 		if(playstatus == LOOSE)
 		{
-			System.out.println("Loose");
+			//System.out.println("Loose");
 			loosestack += 1;
 		}
 		else
 		{
-			System.out.println("Winning");
+			//System.out.println("Winning");
 			winstack += 1;
 		}
 		if(winstack == EVERY_DAY_STAKE/2)
@@ -52,12 +65,20 @@ public class GamblingSimulator {
 		}
 		
 	}  
+		winAmount[i] = winstack;
+	    looseAmount[i] = loosestack;
+	    
+	    System.out.println("winning stack = "+winstack);
+	    System.out.println("loose stack = "+loosestack);
+		System.out.println("-----------------------------");
+		
+		}
 	}
 
 	public static void main(String[] args) {
 		
 		System.out.println("Well Come to Gambling Simulator Project");
-		
+		gameStatus();
 		playgame();
 		
 	
